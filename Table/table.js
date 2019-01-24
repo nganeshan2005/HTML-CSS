39327b7c4138ef686e2d571ed223b756f4fe169e
Ganesh <ganesh.narwane@yahoo.com>
2019-01-25 04:30:14 +0530

    Column resize added successfully

--- Table/table.js
+++ Table/table.js
@@ -4,8 +4,32 @@ let fixedHeader = document.getElementsByClassName('fixed-header');
 let thArray = Array.from(th);
 
 let thHolder = document.querySelectorAll(".holder");
+let targetTh, sliced, mouseMove;
+for(var i = 0; i < thHolder.length; i++){
+    
+    thHolder[i].addEventListener('mousedown', function(e){
+        targetTh = e.target.parentElement.parentElement
+        let computedWidth = window.getComputedStyle(targetTh).width;
+        //since table th width is auto not able to capture its value so assigning computed width
+        let str = computedWidth
+            sliced = parseInt(str.slice(0, -2), 0);
+        let mouseStart = e.clientX;
+        document.addEventListener('mousemove', function(e){
+            mouseTravel = (-mouseStart + e.clientX);
+            targetTh.style.width = (sliced + mouseTravel) + "px";
+            console.log(mouseTravel);
+            
+            return mouseTravel;
+        })
+    })
+    document.addEventListener('mouseup', function(e){
+        e.target.removeEventListener('mousemove', function(e){
+            targetTh.style.width = (sliced + mouseTravel) + "px";
+        })
+    })
+}
 
-let mouseMove;
+/* let mouseMove;
 function calculateMouseMove(){
     let mouseInitialPosition = 0;
     myTable.addEventListener('mousedown', function(e){
@@ -16,18 +40,18 @@ function calculateMouseMove(){
     let mouseNewPosition = 0;
     myTable.addEventListener('mouseup', function(e){
         mouseNewPosition = e.clientX;
-        /* console.log(mouseNewPosition); */
+        console.log(mouseNewPosition);
         mouseMove = mouseNewPosition - mouseInitialPosition;
-        /* console.log(mouseMove); */
+        console.log(mouseMove);
     })
-    /* console.log(mouseMove); */
+    console.log(mouseMove);
     return mouseMove;
 };
 calculateMouseMove();
 
 
 for(var i = 0; i < thHolder.length; i++){
-    thHolder[i].addEventListener('mousedown', function(e){
+    thHolder[i].addEventListener('drag', function(e){
         let targetTH = e.target.parentElement.parentElement;
         let thWidth = window.getComputedStyle(targetTH).width;
         let getCurrentWidth = parseInt(targetTH.style.width);
@@ -40,7 +64,7 @@ for(var i = 0; i < thHolder.length; i++){
         }
             
     )}
-
+ */
 
 
 
